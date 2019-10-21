007be3feb90f2eb4c6461dfe41ba91c6a9640933
Ubuntu <ubuntu@ip-172-31-28-176.ap-southeast-1.compute.internal>
2019-10-21 08:24:53 +0000

    Added checkboxes

--- pymongo-flask/app.py
+++ pymongo-flask/app.py
@@ -20,18 +20,35 @@ app = Flask(__name__)
 def index():
     search_terms = request.args.get('search-by')
     country = request.args.get('country')
+    must_have = request.args.getlist('must-have')
+    
 
     search_criteria = {}
-    
+    print (search_criteria)
     if search_terms is not None and search_terms is not "":
         search_criteria["name"] = re.compile(r'{}'.format(search_terms), re.I)
-    
-    if country is not None and country is not "Any":
+
+    if country != None and country != "Any":
         search_criteria['address.country'] = country 
+        
+    if len(must_have) > 0:
+        search_criteria['amenities'] = {
+            '$all' : must_have 
+        }
+        
+    
+        
+    print (search_criteria)
+    # # for display
+    # if search_terms is None:
+    #     search_terms =""
+    
+    if search_terms is None:
+        search_terms = ""
     
     conn = get_connection()
     cursor = conn[DATABASE_NAME]["listingsAndReviews"].find(search_criteria).limit(10)
-    return render_template("index.template.html", results=cursor)
+    return render_template("index.template.html", results=cursor, search_terms=search_terms, country=country)
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
